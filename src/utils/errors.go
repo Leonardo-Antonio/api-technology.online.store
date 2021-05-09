a3c459180925d5c3b0d33968f00af8f36b99459e
Leonardo-Antonio <leo2001.nl8@gmail.com>
2021-05-08 21:00:47 -0500

    validate error type users and ids

--- src/utils/errors.go
+++ src/utils/errors.go
@@ -3,7 +3,10 @@ package utils
 import "errors"
 
 var (
-	ErrDeleteOne = errors.New("could not delete user")
-	ErrUpdateOne = errors.New("more than one record is updated")
+	ErrDeleteOne         = errors.New("could not delete user")
+	ErrUpdateOne         = errors.New("more than one record is updated")
+	ErrInsertOne         = errors.New("more than one record is inserted")
+	ErrIDRequired        = errors.New("the id field is required")
+	ErrUserNotExists     = errors.New("username does not exist")
+	ErrCategoryNotExists = errors.New("category does not exist")
 )
-
