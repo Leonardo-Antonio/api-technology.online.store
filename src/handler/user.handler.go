685b10e07ef88d909f60ba9863f85ba2e5cc67f5
Leonardo-Antonio <leo2001.nl8@gmail.com>
2021-05-08 21:02:41 -0500

    change in response user

--- src/handler/user.handler.go
+++ src/handler/user.handler.go
@@ -19,24 +19,21 @@ func NewUser(storage model.IUser) *user {
 	return &user{storage}
 }
 
-func (u *user) Create(c *fiber.Ctx) error {
+func (u *user) SignIn(c *fiber.Ctx) error {
 	dataUser := new(entity.User)
 	if err := c.BodyParser(dataUser); err != nil {
-		response := utils.ResponseError(err)
-		return c.Status(http.StatusBadRequest).JSON(response)
+		return c.Status(http.StatusBadRequest).JSON(utils.ResponseError(err))
 	}
 
 	if err := utils.UserValidate(*dataUser); err != nil {
-		response := utils.ResponseError(err)
-		return c.Status(http.StatusBadRequest).JSON(response)
+		return c.Status(http.StatusBadRequest).JSON(utils.ResponseError(err))
 	}
 
 	dataUser.Password = base64.StdEncoding.EncodeToString([]byte(dataUser.Password))
 
 	result, err := u.storage.Create(*dataUser)
 	if err != nil {
-		response := utils.ResponseError(err)
-		return c.Status(http.StatusBadRequest).JSON(response)
+		return c.Status(http.StatusBadRequest).JSON(utils.ResponseError(err))
 	}
 	response := utils.ResponseSatisfactory("user created successfully", result)
 	return c.Status(http.StatusCreated).JSON(response)
@@ -45,53 +42,75 @@ func (u *user) Create(c *fiber.Ctx) error {
 func (u *user) RemoveByID(c *fiber.Ctx) error {
 	objectID, err := primitive.ObjectIDFromHex(c.Params("id"))
 	if err != nil {
-		response := utils.ResponseError(err)
-		return c.Status(http.StatusBadRequest).JSON(response)
+		return c.Status(http.StatusBadRequest).JSON(utils.ResponseError(err))
 	}
 
 	result, err := u.storage.RemoveByID(objectID)
+	if result.MatchedCount != 1 {
+		return c.Status(http.StatusBadRequest).JSON(utils.ResponseError(utils.ErrUserNotExists))
+	}
 	if err != nil {
-		response := utils.ResponseError(err)
-		return c.Status(http.StatusNotFound).JSON(response)
+		return c.Status(http.StatusNotFound).JSON(utils.ResponseError(err))
 	}
 	response := utils.ResponseSatisfactory("user deleted successfully", result)
 	return c.Status(http.StatusOK).JSON(response)
 }
 
-func (u *user) Login(c *fiber.Ctx) error {
+func (u *user) LogIn(c *fiber.Ctx) error {
 	userData := new(entity.User)
 	if err := c.BodyParser(userData); err != nil {
-		response := utils.ResponseError(err)
-		return c.Status(http.StatusBadRequest).JSON(response)
+		return c.Status(http.StatusBadRequest).JSON(utils.ResponseError(err))
 	}
 
 	userData.Password = base64.StdEncoding.EncodeToString([]byte(userData.Password))
 
 	dataUser, err := u.storage.FindOne(*userData)
 	if err != nil {
-		response := utils.ResponseError(err)
-		return c.Status(http.StatusUnauthorized).JSON(response)
+		return c.Status(http.StatusUnauthorized).JSON(utils.ResponseError(err))
 	}
 
 	token, err := autorization.GenerateToken(dataUser)
 	if err != nil {
-		response := utils.ResponseError(err)
-		return c.Status(http.StatusInternalServerError).JSON(response)
+		return c.Status(http.StatusInternalServerError).JSON(utils.ResponseError(err))
 	}
 
 	userDataAndToken := map[string]interface{}{
 		"token": token,
-		"user": userData,
+		"user":  dataUser,
 	}
 	response := utils.ResponseSatisfactory("user found successfully", userDataAndToken)
+	return c.Status(http.StatusOK).JSON(response)
+}
+
+func (u *user) Update(c *fiber.Ctx) error {
+	dataUser := new(entity.User)
+	if err := c.BodyParser(&dataUser); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(utils.ResponseError(err))
+	}
+
+	if dataUser.ID.IsZero() {
+		return c.Status(http.StatusBadRequest).JSON(utils.ResponseError(utils.ErrIDRequired))
+	}
+	if err := utils.UserValidate(*dataUser); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(utils.ResponseError(err))
+	}
+	dataUser.Password = base64.StdEncoding.EncodeToString([]byte(dataUser.Password))
+	result, err := u.storage.UpdateByID(*dataUser)
+	if result.MatchedCount != 1 {
+		return c.Status(http.StatusBadRequest).JSON(utils.ResponseError(utils.ErrUserNotExists))
+	}
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(utils.ResponseError(err))
+	}
+
+	response := utils.ResponseSatisfactory("user updated successfully", result)
 	return c.Status(http.StatusOK).JSON(response)
 }
 
 func (u *user) GetAll(c *fiber.Ctx) error {
 	users, err := u.storage.FindAll()
 	if err != nil {
-		response := utils.ResponseError(err)
-		return c.Status(http.StatusInternalServerError).JSON(response)
+		return c.Status(http.StatusInternalServerError).JSON(utils.ResponseError(err))
 	}
 	response := utils.ResponseSatisfactory("Ok", users)
 	return c.Status(http.StatusOK).JSON(response)
