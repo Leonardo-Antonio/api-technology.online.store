8b161db823e5c400447e49e7aa55fcfc47172643
Leonardo-Antonio <leo2001.nl8@gmail.com>
2021-05-08 21:03:24 -0500

    remove errRequiredField

--- src/utils/userValidation.go
+++ src/utils/userValidation.go
@@ -6,18 +6,28 @@ import (
 	"strings"
 )
 
-func errRequiredField(field string) error {
-	return errors.New("the " + field + " field is required")
-}
-
 func UserValidate(user entity.User) error {
-	if len(user.Name) == 0 { return errRequiredField("name") }
-	if len(user.LastName) == 0 { return errRequiredField("last name") }
-	if len(user.Nick) == 0 { return errRequiredField("nick name") }
-	if len(user.Email) == 0 { return errRequiredField("email") }
-	if !strings.ContainsAny(user.Email, "@") { return errors.New("enter a valid email address") }
-	if len(user.Password) < 8 { return errors.New("enter a strong password") }
-	if len(user.Rol) == 0 { return errRequiredField("rol") }
+	if len(user.Name) == 0 {
+		return errRequiredField("name")
+	}
+	if len(user.LastName) == 0 {
+		return errRequiredField("last name")
+	}
+	if len(user.Nick) == 0 {
+		return errRequiredField("nick name")
+	}
+	if len(user.Email) == 0 {
+		return errRequiredField("email")
+	}
+	if !strings.ContainsAny(user.Email, "@") {
+		return errors.New("enter a valid email address")
+	}
+	if len(user.Password) < 8 {
+		return errors.New("enter a strong password")
+	}
+	if len(user.Rol) == 0 {
+		return errRequiredField("rol")
+	}
 	switch strings.ToUpper(user.Rol) {
 	case "USER":
 		return nil
