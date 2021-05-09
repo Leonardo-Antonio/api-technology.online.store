adec78e34300bb0e7a1884cbdb229f07065be5aa
Leonardo-Antonio <leo2001.nl8@gmail.com>
2021-05-08 21:00:26 -0500

    claim of user

--- src/entity/Claims.go
+++ src/entity/Claims.go
@@ -3,9 +3,9 @@ package entity
 import "github.com/dgrijalva/jwt-go"
 
 type Claim struct {
-	Email string `bson:"email" json:"email" xml:"email"`
-	Name string `bson:"name" json:"name" xml:"name"`
+	Email    string `bson:"email" json:"email" xml:"email"`
+	Name     string `bson:"name" json:"name" xml:"name"`
 	LastName string `bson:"last_name" json:"last_name" xml:"last_name"`
-	Nick string `bson:"nick" json:"nick" xml:"nick"`
+	Nick     string `bson:"nick" json:"nick" xml:"nick"`
 	jwt.StandardClaims
 }
