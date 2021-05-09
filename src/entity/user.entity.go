ba5ef71d57c298b58dee810d0197b2fc16312bc5
Leonardo-Antonio <leo2001.nl8@gmail.com>
2021-05-08 21:03:46 -0500

    order in fields

--- src/entity/user.entity.go
+++ src/entity/user.entity.go
@@ -6,18 +6,18 @@ import (
 )
 
 type User struct {
-	ID primitive.ObjectID `bson:"_id" json:"_id"`
-	Name string `bson:"name,omitempty," json:"name,omitempty"`
-	LastName string `bson:"last_name,omitempty" json:"last_name,omitempty"`
-	Rol string `bson:"rol,omitempty" json:"rol,omitempty"`
-	Nick string `bson:"nick,omitempty" json:"nick,omitempty"`
-	Image string `bson:"image,omitempty" json:"image,omitempty"`
-	Email string `bson:"email,omitempty" json:"email,omitempty"`
-	Password string `bson:"password,omitempty" json:"password,omitempty"`
+	ID        primitive.ObjectID `bson:"_id" json:"_id"`
+	Name      string             `bson:"name,omitempty," json:"name,omitempty"`
+	LastName  string             `bson:"last_name,omitempty" json:"last_name,omitempty"`
+	Rol       string             `bson:"rol,omitempty" json:"rol,omitempty"`
+	Nick      string             `bson:"nick,omitempty" json:"nick,omitempty"`
+	ImageSrc  string             `bson:"image_src" json:"image_src"`
+	Email     string             `bson:"email,omitempty" json:"email,omitempty"`
+	Password  string             `bson:"password,omitempty" json:"password,omitempty"`
 	CreatedBy primitive.ObjectID `bson:"created_by,omitempty" json:"created_by,omitempty"`
 	UpdatedBy primitive.ObjectID `bson:"updated_by,omitempty" json:"updated_by,omitempty"`
 	DeletedBy primitive.ObjectID `bson:"deleted_by,omitempty" json:"deleted_by,omitempty"`
-	CreatedAt time.Time `bson:"created_at,omitempty" json:"created_at,omitempty"`
-	UpdatedAt time.Time `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
-	Active bool `bson:"active,omitempty" json:"active,omitempty"`
+	CreatedAt time.Time          `bson:"created_at,omitempty" json:"created_at,omitempty"`
+	UpdatedAt time.Time          `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
+	Active    bool               `bson:"active,omitempty" json:"active,omitempty"`
 }
