2af8d48dcea0405c9bfca6b48d0aaa7b37f08d9e
Leonardo-Antonio <leo2001.nl8@gmail.com>
2021-05-08 21:01:55 -0500

    fields unique db

--- src/dbutil/restriction.go
+++ src/dbutil/restriction.go
@@ -13,18 +13,18 @@ func CreateIndex(collectionName string, field string, unique bool) bool {
 
 	// 1. Lets define the keys for the index we want to create
 	mod := mongo.IndexModel{
-		Keys: bson.M{field: 1}, // index in ascending order or -1 for descending order
+		Keys:    bson.M{field: 1}, // index in ascending order or -1 for descending order
 		Options: options.Index().SetUnique(unique),
 	}
 
-	// 2. Create the context for this operation
+	// 2. SignIn the context for this operation
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	// 3. Connect to the database and access the collection
 	collection := Connection().Collection(collectionName)
 
-	// 4. Create a single index
+	// 4. SignIn a single index
 	_, err := collection.Indexes().CreateOne(ctx, mod)
 	if err != nil {
 		// 5. Something went wrong, we log it and return false
